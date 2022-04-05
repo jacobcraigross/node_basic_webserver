7cab2f842af2b65c8053a72bfc5178f23f45a86d
Jake Ross <survivaldawn@gmail.com>
2022-04-05 09:02:56 -0500

    Update echo.js

--- echo.js
+++ echo.js
@@ -1,10 +1,17 @@
-// a simple echo function
+// basic Node web server. 
 
-function echo (str, num){
-    for (var i = 0; i < num; i++){
-        console.log(str);
-    }
-}
+const http = require('http');
+const hostname = '127.0.0.1';
+const port = 3000;
 
-echo ("tater", 10);
-echo ("gagaga", 10);
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World');
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
+
+// Now, run your web server using node app.js. Visit http://localhost:3000 and you will see a message saying "Hello World".
